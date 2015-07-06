88d7a2d8221b0b32b4116f43c8de983b43edc63e
Israel Santana <isra@miscorreos.org>
2015-07-06 19:20:46 +0100

    Fix kibana tests

--- spec/kibana_spec.rb
+++ spec/kibana_spec.rb
@@ -1,15 +1,21 @@
 require_relative 'spec_helper'
 
-describe file '/opt/kibana4' do
+describe file '/opt/kibana' do
   it { should be_directory }
 end
 
-describe file '/etc/initd./kibana4' do
+describe file '/etc/init.d/kibana4' do
   it { should be_file }
   it { should exist }
 end
 
 describe service 'kibana4' do
   it { should be_enabled }
+end
+describe service 'kibana' do
   it { should be_running }
+end
+
+describe port 5601 do
+  it { should be_listening }
 end
