f054acfdbed2c196d58ac3eb3214905af21bc445
Israel Santana <isra@miscorreos.org>
2015-07-06 18:53:36 +0100

    Add sudo command to java installer

--- provisioning/java8.sh
+++ provisioning/java8.sh
@@ -4,11 +4,13 @@ if which java >/dev/null; then
     echo "skip java 8 installation"
 else
   echo "java 8 installation"
-  apt-get install --yes python-software-properties
-  add-apt-repository ppa:webupd8team/java
-  apt-get update -qq
+  sudo apt-get update
+  sudo apt-get dist-upgrade -y
+  sudo apt-get install --yes python-software-properties
+  sudo add-apt-repository ppa:webupd8team/java
+  sudo apt-get update -qq
   echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
   echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections
-  apt-get install --yes oracle-java8-installer
-  yes "" | apt-get -f install
+  sudo apt-get install --yes oracle-java8-installer
+  yes "" | sudo apt-get -f install
 fi
